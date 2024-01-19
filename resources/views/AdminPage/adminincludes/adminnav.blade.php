f5fc989255504b565d412326931d9e9acfadbbfe
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-01-19 08:13:46 +0800

    code

--- resources/views/AdminPage/adminincludes/adminnav.blade.php
+++ resources/views/AdminPage/adminincludes/adminnav.blade.php
@@ -2,9 +2,10 @@
     <h3>HunGerZone</h3>
     <nav>
         <a href="{{ route('view.admin') }}">Home</a>
-        <a href="{{ route('view.user')}}">Users</a>
+        <a href="{{ route('view.user') }}">Users</a>
         <a href="{{ route('uploader.login.images') }}">Login Images</a>
         <a href="{{ route('uploader.restaurant') }}">Register New Restaurant</a>
-        <a href="{{ route('uploader.menu') }}" class="last">Register New Menus</a>
+        <a href="{{ route('uploader.menu') }}">Register New Menus</a>
+        <a href="{{ route('view.home') }}" class="last">Client Home</a>
     </nav>
 </header>
--- resources/views/homePages/profile.blade.php
+++ resources/views/homePages/profile.blade.php
@@ -10,10 +10,18 @@
             <label>Email and contact#: </label><span>{{ auth()->user()->email }}/{{ auth()->user()->phone }}</span><br>
         </div>
         <div class="sec-btn">
-            <a href="{{ route('edit.profile', ['id' => encrypt(auth()->user()->id)]) }}"><button
-                    class="btnUpdateprofile">Update
-                    Profile</button></a><br>
-            <a href="{{ route('logout') }}"><button class="btnSignout">Sign Out</button></a>
+            @if (auth()->user()->isAdmin == 1)
+                <a href="{{ route('view.admin') }}"><button class="btnUpdateprofile">Admin</button></a>
+                <a href="{{ route('edit.profile', ['id' => encrypt(auth()->user()->id)]) }}"><button
+                        class="btnUpdateprofile">Update
+                        Profile</button></a><br>
+                <a href="{{ route('logout') }}"><button class="btnSignout">Sign Out</button></a>
+            @else
+                <a href="{{ route('edit.profile', ['id' => encrypt(auth()->user()->id)]) }}"><button
+                        class="btnUpdateprofile">Update
+                        Profile</button></a><br>
+                <a href="{{ route('logout') }}"><button class="btnSignout">Sign Out</button></a>
+            @endif
         </div>
     </div>
 @stop
